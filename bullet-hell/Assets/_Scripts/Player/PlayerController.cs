7538081b490b7d06391eeaf088bc9df3fb593dd0
chris8332558 <chris8332558@gmail.com>
2024-11-02 13:24:20 -0700

    Add HandleMoveAnim() that can play corresponding moving animation

--- bullet-hell/Assets/_Scripts/Player/PlayerController.cs
+++ bullet-hell/Assets/_Scripts/Player/PlayerController.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    private Vector2 direction;
+
     private Rigidbody2D body;
     private SpriteRenderer sprite;
-    private Vector2 direction;
+    private Animator anim;
+
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        body.bodyType = RigidbodyType2D.Kinematic;
+        body.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
     }
+
 
     private void Update()
     {
@@ -19,6 +27,7 @@ public class PlayerController : MonoBehaviour
         body.velocity = direction * moveSpeed;
 
         HandleFlip();
+        HandleMoveAnim();
     }
 
     private void HandleFlip()
@@ -32,4 +41,46 @@ public class PlayerController : MonoBehaviour
             sprite.flipX = false;
 		}
 	}
+
+    private void HandleMoveAnim()
+    {
+        if (Mathf.Abs(direction.x) > 0)
+        {
+            if (direction.y > 0)
+            {
+                anim.Play("RunNE");
+                Debug.Log("NE");
+			}
+            else if (direction.y < 0)
+            {
+                anim.Play("RunSE");
+                Debug.Log("SE");
+			}
+            else 
+			{
+                anim.Play("RunE");
+                Debug.Log("E");
+            }
+        }
+        else 
+		{ 
+		    if (direction.y > 0)
+            {
+                anim.Play("RunN");
+                Debug.Log("N");
+			}
+            else if (direction.y < 0)
+            {
+                anim.Play("RunS");
+                Debug.Log("S");
+			}
+            else
+            {
+                anim.Play("Idle");   
+                Debug.Log("Idle");
+			}
+		}
+
+
+    }
 }
