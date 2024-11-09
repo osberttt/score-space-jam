b033c697c8c13dd0053a92238934e9427bf64156
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2024-11-10 02:09:49 +0700

    Add more explosions

--- Doomsday Kickoff/Assets/_Scripts/BOMB.cs
+++ Doomsday Kickoff/Assets/_Scripts/BOMB.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 using Core;
 public class BOMB : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    [SerializeField] private ParticleSystem explosion;
     [SerializeField] private AudioClip explosionSfx;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(Constants.Tags.Player))
         {
             AudioManager.Instance.PlaySoundFXClip(explosionSfx, transform, 60f);
-            EventManager.InvokeEvent(GameplayEvent.GameOver);
+            sr.enabled = false;
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            StartCoroutine(BombCo());
         }
+    }
+
+    IEnumerator BombCo()
+    {
+        yield return new WaitForSeconds(2f);
+        EventManager.InvokeEvent(GameplayEvent.GameOver);
     }
 }
--- Doomsday Kickoff/Assets/_Scripts/Health.cs
+++ Doomsday Kickoff/Assets/_Scripts/Health.cs
@@ -6,17 +6,17 @@ using System;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] private AudioClip explosionSfx;
+    [SerializeField] private ParticleSystem explosion;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(Constants.Tags.Player))
         {
+            AudioManager.Instance.PlaySoundFXClip(explosionSfx, transform, 60f);
+            Instantiate(explosion, transform.position, Quaternion.identity);
             collision.GetComponent<PlayerHit>().RestoreHealth(1f);
-            DestroySelf();
+            Destroy(gameObject);
         }
-    }
-
-    private void DestroySelf()
-    {
-        Destroy(gameObject);
     }
 }
